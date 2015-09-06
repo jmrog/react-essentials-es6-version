d5f2653c19efe7ce66faa6fc7f43b6e0e4eefc89
Jason Rogers <jason@ascendiv.com>
2015-09-05 21:47:29 -0400

    Adds ch02 files

---
+++ ch02/source/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const listOfItems = <ul className="list-of-items">
+                        <li className="item-1">Item 1</li>
+                        <li className="item-2">Item 2</li>
+                        <li className="item-3">Item 3</li>
+                    </ul>;
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
+
